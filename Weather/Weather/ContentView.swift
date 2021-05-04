220f48744f4a05e7ee5ddaa2891c892e431d08a8
Radoslaw Mikolajczyk <radoslaw.mikolajczyk@o2.pl>
2021-05-04 14:53:07 +0200

    First commit

---
+++ Weather/Weather/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Weather
+//
+//  Created by AppleLab on 04/05/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Weather/Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by AppleLab on 04/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
